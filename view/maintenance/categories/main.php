d743dc987f1ccba2358e141d7624b95fc2411141
jajapabatao <headshot99001@gmail.com>
2017-08-21 19:45:16 -0700

    update

--- view/maintenance/categories/main.php
+++ view/maintenance/categories/main.php
@@ -1,4 +1,4 @@
-<?php
+<?php include('../../../controller/master/log.php');
 
 $types=$_GET["types"];
 echo'<input type="hidden" id="type" value="'.$types.'">';
--- view/maintenance/employee/main.php
+++ view/maintenance/employee/main.php
@@ -1,3 +1,4 @@
+<?php include('../../../controller/master/log.php');?>
 
 
 <!---->
